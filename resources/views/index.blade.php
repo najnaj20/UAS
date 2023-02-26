0c16f4fef7f50132d132eba48137bbf64439fe4e
najnaj20 <portofnajwa@gmail.com>
2023-02-26 23:16:17 +0800

    aplikasi rakit computer

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<h1>List of Users</h1>
+
+<table>
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($users as $user)
+        <tr>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->role }}</td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
